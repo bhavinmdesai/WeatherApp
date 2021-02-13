40ea7a29dc267158b892e375c6245c39c1f7b654
Bhavin Desai <bhavinmdesai@gmail.com>
2021-02-13 21:08:02 +0530

    Removed unwanted properties

--- viewmodels/src/main/java/dev/bhavindesai/viewmodels/WeatherListViewModel.kt
+++ viewmodels/src/main/java/dev/bhavindesai/viewmodels/WeatherListViewModel.kt
@@ -1,21 +1,18 @@
 package dev.bhavindesai.viewmodels
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import dev.bhavindesai.data.repositories.WeatherRepository
-import dev.bhavindesai.domain.Location
+import kotlinx.coroutines.FlowPreview
 import javax.inject.Inject
 
 class WeatherListViewModel @Inject constructor(
     weatherRepository: WeatherRepository
 ) : ViewModel() {
 
-    private val _weatherForecast = weatherRepository
+    @FlowPreview
+    val weatherForecast = weatherRepository
         .getWeatherForCity("london")
         .asLiveData(viewModelScope.coroutineContext)
-
-    val weatherForecast: LiveData<Location>
-        get() = _weatherForecast
 }
