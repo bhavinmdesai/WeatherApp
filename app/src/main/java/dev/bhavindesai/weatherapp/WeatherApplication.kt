4adab8ec13eb4915ad9bae37d91af70a8c26df38
Bhavin Desai <bhavinmdesai@gmail.com>
2021-02-13 18:17:06 +0530

    Injected application class

--- app/src/main/java/dev/bhavindesai/weatherapp/WeatherApplication.kt
+++ app/src/main/java/dev/bhavindesai/weatherapp/WeatherApplication.kt
@@ -1,10 +1,25 @@
 package dev.bhavindesai.weatherapp
 
 import android.app.Application
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import dev.bhavindesai.weatherapp.di.DaggerAppComponent
+import javax.inject.Inject
 
-class WeatherApplication : Application() {
+class WeatherApplication : Application(), HasAndroidInjector {
+
+    @Inject
+    lateinit var androidInjector: DispatchingAndroidInjector<Any>
 
     override fun onCreate() {
         super.onCreate()
+
+        DaggerAppComponent
+            .builder()
+            .application(this)
+            .build()
+            .inject(this)
     }
+
+    override fun androidInjector() = androidInjector
 }
