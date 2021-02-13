598bba4bfb72adddab8aa12016f0163de448c354
Bhavin Desai <bhavinmdesai@gmail.com>
2021-02-14 01:22:35 +0530

    Implemented internet util

---
+++ data/src/main/java/dev/bhavindesai/data/utils/InternetUtil.kt
@@ -0,0 +1,47 @@
+package dev.bhavindesai.data.utils
+
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
+object InternetUtil {
+
+    private lateinit var application: Application
+
+    fun init(application: Application) {
+        InternetUtil.application = application
+    }
+
+    fun isInternetOn(): Boolean {
+        var result = false
+        val connectivityManager =
+            application.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val networkCapabilities = connectivityManager.activeNetwork ?: return false
+            val actNw =
+                connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+            result = when {
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        } else {
+            connectivityManager.run {
+                connectivityManager.activeNetworkInfo?.run {
+                    result = when (type) {
+                        ConnectivityManager.TYPE_WIFI -> true
+                        ConnectivityManager.TYPE_MOBILE -> true
+                        ConnectivityManager.TYPE_ETHERNET -> true
+                        else -> false
+                    }
+
+                }
+            }
+        }
+
+        return result
+    }
+}
