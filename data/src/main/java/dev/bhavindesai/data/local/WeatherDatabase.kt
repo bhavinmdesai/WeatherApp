06f5d008d282746b47345f845b7f519941f4b1d1
Bhavin Desai <bhavinmdesai@gmail.com>
2021-02-14 01:20:41 +0530

    Added entity

--- data/src/main/java/dev/bhavindesai/data/local/WeatherDatabase.kt
+++ data/src/main/java/dev/bhavindesai/data/local/WeatherDatabase.kt
@@ -4,8 +4,9 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import dev.bhavindesai.domain.local.Location
 import dev.bhavindesai.domain.local.Weather
+import dev.bhavindesai.domain.remote.WhereOnEarth
 
-@Database(entities = [Location::class, Weather::class], version = 1, exportSchema = false)
+@Database(entities = [Location::class, Weather::class, WhereOnEarth::class], version = 1, exportSchema = false)
 abstract class WeatherDatabase : RoomDatabase() {
 
     abstract fun weatherDataDao(): WeatherDataDao
