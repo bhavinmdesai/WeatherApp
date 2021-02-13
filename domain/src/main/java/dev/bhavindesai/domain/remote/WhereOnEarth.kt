96de53488d93a2cb65476ebd832c124ddeb8661d
Bhavin Desai <bhavinmdesai@gmail.com>
2021-02-14 01:18:39 +0530

    Tagged as entity and changed property name

--- domain/src/main/java/dev/bhavindesai/domain/remote/WhereOnEarth.kt
+++ domain/src/main/java/dev/bhavindesai/domain/remote/WhereOnEarth.kt
@@ -1,8 +1,13 @@
 package dev.bhavindesai.domain.remote
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class WhereOnEarth (
     val title: String,
-    val locationType: String,
+    val location_type: String,
+    @PrimaryKey
     val woeid: Long,
-    val lattLong: String
+    val latt_long: String
 )
