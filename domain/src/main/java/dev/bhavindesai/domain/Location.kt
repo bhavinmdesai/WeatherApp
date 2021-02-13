0f65a6f087d5a022968af55b2648006080d9e946
Bhavin Desai <bhavinmdesai@gmail.com>
2021-02-13 18:18:26 +0530

    Removed unwanted properties

--- domain/src/main/java/dev/bhavindesai/domain/Location.kt
+++ domain/src/main/java/dev/bhavindesai/domain/Location.kt
@@ -5,14 +5,8 @@ data class Location (
     val time: String,
     val sunRise: String,
     val sunSet: String,
-    val timezoneName: String,
-    val parent: Parent,
-    val sources: List<Source>,
     val title: String,
-    val locationType: String,
     val woeid: Long,
-    val lattLong: String,
-    val timezone: String
 )
 
 data class ConsolidatedWeather (
@@ -33,16 +27,16 @@ data class ConsolidatedWeather (
     val predictability: Long
 )
 
-data class Parent (
-    val title: String,
-    val locationType: String,
-    val woeid: Long,
-    val lattLong: String
-)
-
-data class Source (
-    val title: String,
-    val slug: String,
-    val url: String,
-    val crawlRate: Long
-)
+//data class Parent (
+//    val title: String,
+//    val locationType: String,
+//    val woeid: Long,
+//    val lattLong: String
+//)
+//
+//data class Source (
+//    val title: String,
+//    val slug: String,
+//    val url: String,
+//    val crawlRate: Long
+//)
