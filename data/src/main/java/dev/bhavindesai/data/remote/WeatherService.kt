5daa649a94031a59e0c98ef6965b05de18d7d9cb
Bhavin Desai <bhavinmdesai@gmail.com>
2021-02-13 18:15:20 +0530

    Added endpoint path

--- data/src/main/java/dev/bhavindesai/data/remote/WeatherService.kt
+++ data/src/main/java/dev/bhavindesai/data/remote/WeatherService.kt
@@ -3,13 +3,15 @@ package dev.bhavindesai.data.remote
 import dev.bhavindesai.domain.Location
 import dev.bhavindesai.domain.WhereOnEarth
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface WeatherService {
 
-    @GET
-    fun getWhereOnEarth(city: String) : List<WhereOnEarth>
+    @GET("location/search")
+    suspend fun getWhereOnEarth(@Query("query") city: String) : List<WhereOnEarth>
 
-    @GET
-    fun getWeatherData(woeId: Long) : List<Location>
+    @GET("location/{woeId}")
+    fun getWeatherData(@Path("woeId") woeId: Long) : List<Location>
 
 }
