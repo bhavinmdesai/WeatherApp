fba3952232a72166de87462c22991252094d3ed5
Bhavin Desai <bhavinmdesai@gmail.com>
2021-02-14 01:19:56 +0530

    Implemented observer

--- app/src/main/java/dev/bhavindesai/weatherapp/ui/weatherlist/WeatherListFragment.kt
+++ app/src/main/java/dev/bhavindesai/weatherapp/ui/weatherlist/WeatherListFragment.kt
@@ -5,26 +5,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import dev.bhavindesai.viewmodels.WeatherListViewModel
-import dev.bhavindesai.weatherapp.R
+import dev.bhavindesai.weatherapp.databinding.WeatherListFragmentBinding
 import dev.bhavindesai.weatherapp.ui.base.BaseFragment
+import kotlinx.coroutines.FlowPreview
 
 class WeatherListFragment : BaseFragment() {
 
     private val viewModel: WeatherListViewModel by lazyViewModel()
+    private lateinit var binding: WeatherListFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.weather_list_fragment, container, false)
+    ): View {
+        binding = WeatherListFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    @FlowPreview
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.weatherForecast.observe(viewLifecycleOwner) {
-            println(it)
+        viewModel.weatherForecast.observe(viewLifecycleOwner) { locationWeatherData ->
+            if (locationWeatherData != null) {
+                binding.groupSuccessfulResponse.visibility = View.VISIBLE
+                binding.groupUnsuccessfulResponse.visibility = View.GONE
+
+                binding.txtCityName.text = locationWeatherData.locationData.title
+                binding.rvWeatherList.adapter = WeatherListAdapter(locationWeatherData)
+            } else {
+                binding.groupSuccessfulResponse.visibility = View.GONE
+                binding.groupUnsuccessfulResponse.visibility = View.VISIBLE
+            }
         }
     }
 }
