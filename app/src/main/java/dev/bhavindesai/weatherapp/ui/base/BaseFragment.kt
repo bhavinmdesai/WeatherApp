f9b0d88945251d384b6563bf41db5f5fbcf886b1
Bhavin Desai <bhavinmdesai@gmail.com>
2021-02-13 18:20:19 +0530

    Added viewModelFactory

--- app/src/main/java/dev/bhavindesai/weatherapp/ui/base/BaseFragment.kt
+++ app/src/main/java/dev/bhavindesai/weatherapp/ui/base/BaseFragment.kt
@@ -1,5 +1,27 @@
 package dev.bhavindesai.weatherapp.ui.base
 
+import android.content.Context
+import androidx.annotation.CallSuper
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import dagger.android.support.AndroidSupportInjection
+import javax.inject.Inject
 
-open class BaseFragment : Fragment()
+open class BaseFragment : Fragment() {
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    /**
+     * Lazily get the view model
+     */
+    inline fun <reified VM : ViewModel> lazyViewModel() = lazy {
+        ViewModelProvider(this, viewModelFactory).get(VM::class.java)
+    }
+
+    @CallSuper
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        AndroidSupportInjection.inject(this)
+    }
+}
