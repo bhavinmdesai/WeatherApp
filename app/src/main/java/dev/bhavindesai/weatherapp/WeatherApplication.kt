a61c8633c630bb518111cc33e1f259aacc2746d6
Bhavin Desai <bhavinmdesai@gmail.com>
2021-02-13 15:28:31 +0530

    Added application class

---
+++ app/src/main/java/dev/bhavindesai/weatherapp/WeatherApplication.kt
@@ -0,0 +1,10 @@
+package dev.bhavindesai.weatherapp
+
+import android.app.Application
+
+class WeatherApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
