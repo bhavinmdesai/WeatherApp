ce921cfda42ed841527d1068778c24bbff512584
Bhavin Desai <bhavinmdesai@gmail.com>
2021-02-13 16:12:22 +0530

    Implemented repository to get WhereOnEarth and WeatherData

--- data/src/main/java/dev/bhavindesai/data/WeatherRepository.kt
+++ data/src/main/java/dev/bhavindesai/data/WeatherRepository.kt
@@ -1,18 +1,46 @@
 package dev.bhavindesai.data
 
-class WeatherRepository {
+import dev.bhavindesai.data.remote.WeatherService
+import dev.bhavindesai.data.sources.MultiDataSource
+import dev.bhavindesai.data.sources.RemoteDataSource
+import dev.bhavindesai.domain.Location
+import dev.bhavindesai.domain.WhereOnEarth
+import kotlinx.coroutines.flow.Flow
 
-    suspend fun getWeatherForCity(city: String) {
-        val woeId = getWhereOnEarthId(city)
+class WeatherRepository(
+    private val weatherService: WeatherService
+) {
 
-        getWeatherForWoeId(woeId)
+    suspend fun getWeatherForCity(city: String) : Flow<Location> {
+        val whereOnEarthList = getWhereOnEarthId(city)
+
+        return getWeatherForWoeId(whereOnEarthList.first().woeid)
     }
 
-    private suspend fun getWhereOnEarthId(city: String): Long {
-        return 44418
+    private suspend fun getWhereOnEarthId(city: String) = rdsWhereOnEarth.getRemoteData(city)
+
+    private fun getWeatherForWoeId(woeId: Long) = mdsWeather.fetch(woeId)
+
+    private val rdsWhereOnEarth = object : RemoteDataSource<String, List<WhereOnEarth>> {
+        override suspend fun getRemoteData(
+            requestData: String
+        ): List<WhereOnEarth> = weatherService.getWhereOnEarth(requestData)
     }
 
-    private suspend fun getWeatherForWoeId(woeId: Long) {
+    private val mdsWeather = object : MultiDataSource<Location, Long, List<Location>>() {
+        override fun mapper(remoteData: List<Location>): Location {
+            return remoteData.first()
+        }
 
+        override suspend fun getLocalData(): Location {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun storeLocalData(data: Location) {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun getRemoteData(requestData: Long): List<Location> =
+            weatherService.getWeatherData(requestData)
     }
 }
