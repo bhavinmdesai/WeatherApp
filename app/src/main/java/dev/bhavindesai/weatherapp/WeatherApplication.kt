783fa90f894a528c8e9fbe0c60c0514bd8a6a779
Bhavin Desai <bhavinmdesai@gmail.com>
2021-02-14 01:20:53 +0530

    Initialized internet util

--- app/src/main/java/dev/bhavindesai/weatherapp/WeatherApplication.kt
+++ app/src/main/java/dev/bhavindesai/weatherapp/WeatherApplication.kt
@@ -5,6 +5,7 @@ import com.facebook.drawee.backends.pipeline.Fresco
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
 import dev.bhavindesai.weatherapp.di.DaggerAppComponent
+import dev.bhavindesai.data.utils.InternetUtil
 import javax.inject.Inject
 
 class WeatherApplication : Application(), HasAndroidInjector {
@@ -16,6 +17,7 @@ class WeatherApplication : Application(), HasAndroidInjector {
         super.onCreate()
 
         Fresco.initialize(this)
+        InternetUtil.init(this)
 
         DaggerAppComponent
             .builder()
