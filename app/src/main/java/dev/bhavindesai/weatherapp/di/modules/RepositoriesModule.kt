f87d21d620a8d3f06b9aa9b62b58738b59cde617
Bhavin Desai <bhavinmdesai@gmail.com>
2021-02-14 18:20:24 +0530

    Added internet util to repository

--- app/src/main/java/dev/bhavindesai/weatherapp/di/modules/RepositoriesModule.kt
+++ app/src/main/java/dev/bhavindesai/weatherapp/di/modules/RepositoriesModule.kt
@@ -5,6 +5,7 @@ import dagger.Provides
 import dev.bhavindesai.data.local.WeatherDataDao
 import dev.bhavindesai.data.repositories.WeatherRepository
 import dev.bhavindesai.data.remote.WeatherService
+import dev.bhavindesai.data.utils.InternetUtil
 import javax.inject.Singleton
 
 @Module
@@ -13,6 +14,7 @@ class RepositoriesModule {
     @[Provides Singleton]
     fun providesWeatherRepository(
         weatherService: WeatherService,
-        weatherDataDao: WeatherDataDao
-    ) = WeatherRepository(weatherService, weatherDataDao)
+        weatherDataDao: WeatherDataDao,
+        internetUtil: InternetUtil
+    ) = WeatherRepository(weatherService, weatherDataDao, internetUtil)
 }
