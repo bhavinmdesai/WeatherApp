a8b0442cde193309d84f05f0ba9ed0270e20ed76
Bhavin Desai <bhavinmdesai@gmail.com>
2021-02-14 21:46:34 +0530

    Added more states

--- viewmodels/src/main/java/dev/bhavindesai/viewmodels/WeatherListViewModel.kt
+++ viewmodels/src/main/java/dev/bhavindesai/viewmodels/WeatherListViewModel.kt
@@ -15,11 +15,34 @@ class WeatherListViewModel @Inject constructor(
     private val mldWeatherForecast = MutableLiveData<LocationWeatherData?>()
     val weatherForecast: LiveData<LocationWeatherData?> = mldWeatherForecast
 
+    private val mldShowProgress = MutableLiveData<Boolean>()
+    val showProgress: LiveData<Boolean> = mldShowProgress
+
+    private val mldShowWeatherList = MutableLiveData<Boolean>()
+    val showWeatherList: LiveData<Boolean> = mldShowWeatherList
+
+    private val mldShowNoInternet = MutableLiveData<Boolean>()
+    val showNoInternet: LiveData<Boolean> = mldShowNoInternet
+
+    private val mldCityName = MutableLiveData<String>()
+    val cityName: LiveData<String> = mldCityName
+
     @FlowPreview
     fun fetchWeather() {
+        mldShowProgress.value = true
+        mldShowWeatherList.value = false
+        mldShowNoInternet.value = false
+        mldCityName.value = ""
+
         viewModelScope.launch {
             weatherRepository.getWeatherForCity("london").collect {
+                mldShowProgress.value = false
                 mldWeatherForecast.value = it
+                mldShowWeatherList.value = it != null
+                mldShowNoInternet.value = it == null
+                it?.let { notNullLocationWeatherData ->
+                    mldCityName.value = notNullLocationWeatherData.locationData.title
+                }
             }
         }
     }
