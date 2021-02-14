b4dd2b63f6627104c3e8841085d3aa791d628fe7
Bhavin Desai <bhavinmdesai@gmail.com>
2021-02-14 18:23:29 +0530

    Added method to fetch weather

--- viewmodels/src/main/java/dev/bhavindesai/viewmodels/WeatherListViewModel.kt
+++ viewmodels/src/main/java/dev/bhavindesai/viewmodels/WeatherListViewModel.kt
@@ -1,18 +1,26 @@
 package dev.bhavindesai.viewmodels
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import dev.bhavindesai.data.repositories.WeatherRepository
+import dev.bhavindesai.domain.local.LocationWeatherData
 import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class WeatherListViewModel @Inject constructor(
-    weatherRepository: WeatherRepository
+    private val weatherRepository: WeatherRepository
 ) : ViewModel() {
 
+    private val mldWeatherForecast = MutableLiveData<LocationWeatherData?>()
+    val weatherForecast: LiveData<LocationWeatherData?> = mldWeatherForecast
+
     @FlowPreview
-    val weatherForecast = weatherRepository
-        .getWeatherForCity("london")
-        .asLiveData(viewModelScope.coroutineContext)
+    fun fetchWeather() {
+        viewModelScope.launch {
+            weatherRepository.getWeatherForCity("london").collect {
+                mldWeatherForecast.value = it
+            }
+        }
+    }
 }
